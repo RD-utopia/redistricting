8e321a026b7c9f60cf6ff16c96a8628c4cf3daf3
Kelzihar <kelzihar@gmail.com>
2021-11-16 01:11:49 -0500

    Changing of VoterTest.java to work be JUnit5 - addition of comments for the tests

--- src/swdmt/redistricting/VoterTest.java
+++ src/swdmt/redistricting/VoterTest.java
@@ -1,6 +1,7 @@
 package swdmt.redistricting;
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.Test;
 /**
  * Tests for Voter objects.
@@ -9,33 +10,47 @@ import org.junit.Test;
  * @version 20191006
  */
 public class VoterTest {
+    /**
+    * Voter's party affiliation should match Party.None when passed Party.None
+    */
     @Test
     public void affiliationAttributeSpecifiedTest() {
         Voter voter1 = new Voter(Party.NONE, new Location(0, 0));
-        assertThat(voter1.affiliation(), is(Party.NONE));
+        assertEquals(voter1.affiliation(), Party.NONE);
     }
 
+    /**
+    * Voter's party affiliation should be Party.None when passed null
+    */
     @Test
     public void affiliationAttributeNullTest() {
         Voter voter2 = new Voter(null,
                                  new Location(Integer.MIN_VALUE,
                                               Integer.MAX_VALUE));
-        assertThat(voter2.affiliation(), is(Party.NONE));
+        assertEquals(voter2.affiliation(), Party.NONE);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void invalidLocationTest() {
-        Voter voter1 = new Voter(Party.NONE, null);
+    /**
+    * When passed a null location should throw an illegal argument exception
+    */
+    @Test
+    public void invalidLocationTest() throws IllegalArgumentException {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Voter voter1 = new Voter(Party.NONE, null);
+        });
     }
 
+    /**
+    * When passed various location attributes they match expected values
+    */
     @Test
     public void locationAttributeTest() {
         Voter voter1 = new Voter(Party.NONE, new Location(0, 0));
-        assertThat(voter1.location(), is(new Location(0, 0)));
+        assertEquals(voter1.location(), new Location(0, 0));
         Voter voter2 = new Voter(null,
                                  new Location(Integer.MIN_VALUE,
                                               Integer.MAX_VALUE));
-        assertThat(voter2.location().xCoordinate(), is(Integer.MIN_VALUE));
-        assertThat(voter2.location().yCoordinate(), is(Integer.MAX_VALUE));
+        assertEquals(voter2.location().xCoordinate(), Integer.MIN_VALUE);
+        assertEquals(voter2.location().yCoordinate(), Integer.MAX_VALUE);
     }
 }
