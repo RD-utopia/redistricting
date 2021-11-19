7e29242593c4704199fd483897d2994f2264e2aa
Kelzihar <kelzihar@gmail.com>
2021-11-19 15:42:00 -0500

    Modification of the VoterTest.java

--- src/swdmt/redistricting/VoterTest.java
+++ src/swdmt/redistricting/VoterTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
  * Tests for Voter objects.
  *
  * @author  Dr. Jody Paul
- * @version 20191006
+ * @version 20211119
  */
 public class VoterTest {
     /**
@@ -46,11 +46,13 @@ public class VoterTest {
     @Test
     public void locationAttributeTest() {
         Voter voter1 = new Voter(Party.NONE, new Location(0, 0));
-        assertEquals(voter1.location(), new Location(0, 0));
         Voter voter2 = new Voter(null,
                                  new Location(Integer.MIN_VALUE,
                                               Integer.MAX_VALUE));
-        assertEquals(voter2.location().xCoordinate(), Integer.MIN_VALUE);
-        assertEquals(voter2.location().yCoordinate(), Integer.MAX_VALUE);
+        assertAll("locations",
+            () -> assertEquals(voter1.location(), new Location(0, 0)),
+            () -> assertEquals(voter2.location().xCoordinate(), Integer.MIN_VALUE),
+            () -> assertEquals(voter2.location().yCoordinate(), Integer.MAX_VALUE)
+        );
     }
 }
